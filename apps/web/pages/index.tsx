3c75085e5e06c0ca14ecc03ee5d9bee5ab6600e2
Luke <xlukexevansx@gmail.com>
2022-09-03 16:32:43 +0100

    quickly populating website

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,9 +2,11 @@ import { Button } from "ui";
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
+    <div className="Home">
+      <div className="Home_content">
+      <h1 className='Home_title'>Lukee.dev</h1>
+      <p className='Home_sub-title'>Hi <span className="Home_wave">👋</span>, my name is Luke a Frontend Engineer at Huel</p>
+      </div>
     </div>
   );
 }
