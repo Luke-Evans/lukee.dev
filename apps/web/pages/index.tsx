5563a8b1bb84bf478da53ab8821b027096460004
Luke <xlukexevansx@gmail.com>
2022-11-27 19:45:49 +0000

    finally updating home styling a lil

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,12 +1,11 @@
-import { Button } from "ui";
-
 export default function Web() {
   return (
     <div className="Home">
-      <div className="Home_content">
-      <h1 className='Home_title'>Lukee.dev</h1>
-      <p className='Home_sub-title'>Hi <span className="Home_wave">👋</span>, my name is Luke a Frontend Engineer at Huel</p>
-      </div>
+      <header>
+        <h1 className="Home_title">
+          Hi, my name is Luke, a Frontend Engineer.
+        </h1>
+      </header>
     </div>
   );
 }
